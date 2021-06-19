7d1de006d0d2aba11ed11c010867f6fba6474834
saschak07 <saschak07@gmail.com>
2021-06-19 20:06:08 +0530

    comments and refactors

--- src/routers/product.js
+++ src/routers/product.js
@@ -12,24 +12,6 @@ const {priceHighToLowComparator,priceLowToHighComparator} = require('../util/pri
 const router = express.Router()
 
 /**
- * this is a dummy get endpoint to be used only once for loading product data
- * to the database for the first time.
- */
-router.get('/spocket/loadData', async (req,res) => {
-    try{
-        const response = await axios.get('https://s3.amazonaws.com/spocket.assets/products.json')
-        response.data.forEach(async (element) => {
-            const product = new Product(element)
-            await product.save()
-        });          
-        res.status(200).send('success')
-    }catch(e){
-        console.log(e)
-        res.status(500).send({errMsg: e})
-    }
-})
-
-/**
  * main product details retrieval endpoint to display list 
  * of all products filtered based on the search criteria
  * default get call: /spocket/items : fetches all items
@@ -47,49 +29,64 @@ router.get('/spocket/items', async (req,res) => {
          * in memory cache is implement below , if there are same query triggered
          * 100 s ago, the cached result will be returned instead of querying the db
          */
-        const cachedResult = await myCache.get(queryString)
+        const cachedResult = myCache.get(queryString)
         if(cachedResult){
             console.log('result being retreived from in memory cache')
             res.status(200).send(cachedResult)
             return
         }
+        /**
+         * creating db query out of the request params
+         */
         Object.keys(req.query).forEach(key =>{
             query[key] = req.query[key]
         })
         let product = []
+        /**
+         * for query with product title, adding regex and case insensitive search 
+         */
         if('title' in query){
-            console.log(`query being triggered to dabase : ${queryString}`)
             const titleSearch = query.title;
             query.title = {$regex: titleSearch,$options: 'i'}    
         }
+        /**
+         * enabling price filter to get product with max/min price 
+         * from the retrieved records, filtered with all other queries
+         */
         if('price' in query){
             if(query.price === 'max'){
                 delete query.price
                 let retrievedProduct = await Product.find(query)
-                const productWithMaxPrice = retrievedProduct.reduce((a,b)=>a.price>b.price?a:b)
-                product=retrievedProduct.filter(data=>data === productWithMaxPrice)
-                await myCache.set(queryString,product,100)
-                res.status(200).send(product)
+                product.push(retrievedProduct.reduce((a,b)=>a.price>b.price?a:b))   
             }
             else{
                 delete query.price
                 let retrievedProduct = await Product.find(query)
-                const productWithMaxPrice = retrievedProduct.reduce((a,b)=>a.price<b.price?a:b)
-                product=retrievedProduct.filter(data=>data === productWithMaxPrice)
-                await myCache.set(queryString,product,100)
-                res.status(200).send(product)
+                product.push(retrievedProduct.reduce((a,b)=>a.price<b.price?a:b))
             }
+            myCache.set(queryString,product,100)
+            res.status(200).send(product)
             return
         }
+        /**
+         * enabling filter for ship_to location, if a ship_to country is selected
+         * the product list will be returned where the shipping_exclusion field 
+         * does not include the ship_to country
+         */
         if('ship_to' in query){
             const shipTo = query.ship_to
             delete query.ship_to
             let retrievedProduct = await Product.find(query)
             product = retrievedProduct.filter(data=>!data.shipping_exclusions.includes(shipTo))
-            await myCache.set(queryString,product,100)
+            myCache.set(queryString,product,100)
             res.status(200).send(product)
             return
         }
+        /**
+         * enabling sorting by price
+         * if sortBy=high reslt returns list of products sorted with price high to low
+         * else product list gets sorted with price low to high
+         */
         if('sortBy' in query){
             const sortBy = query.sortBy
             delete query.sortBy
@@ -100,7 +97,7 @@ router.get('/spocket/items', async (req,res) => {
             else{
                 product.sort(priceHighToLowComparator)
             }
-            await myCache.set(queryString,product,100)
+            myCache.set(queryString,product,100)
             res.status(200).send(product)
             return
         }
@@ -111,15 +108,33 @@ router.get('/spocket/items', async (req,res) => {
         /**
          * after the data fetched from db, same is loaded in the in-mem cache
          * with the queryString as key and retreived data as value with a life time
-         * of 100 seconds to be used by calls made with the same query
+         * of 100 seconds to be used by subsequent calls made with the same query
          *
          */
-        await myCache.set(queryString,product,100)
+        myCache.set(queryString,product,100)
         res.status(200).send(product)
         return
     }catch(e){
         console.log(e)
         res.status(500)
+    }
+})
+
+/**
+ * this is a dummy get endpoint to be used only once for loading product data
+ * to the database for the first time.
+ */
+ router.get('/spocket/loadData', async (req,res) => {
+    try{
+        const response = await axios.get('https://s3.amazonaws.com/spocket.assets/products.json')
+        response.data.forEach(async (element) => {
+            const product = new Product(element)
+            await product.save()
+        });          
+        res.status(200).send('success')
+    }catch(e){
+        console.log(e)
+        res.status(500).send({errMsg: e})
     }
 })
 
